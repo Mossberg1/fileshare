23f6598063b6430f5205e07ce48b2b40f7e01c4e
William Mossberg <william.mossberg@outlook.com>
2026-01-26 21:01:19 +0100

    Added support for file transfer

--- frontend/src/pages/Session.tsx
+++ frontend/src/pages/Session.tsx
@@ -2,9 +2,25 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../context/SocketContext";
 
+interface FileRequestMessage {
+    type: 'file-request';
+    name: string;
+    size: number;
+}
+
+interface FileAcceptMessage {
+    type: 'file-accept';
+}
+
+interface FileRejectMessage {
+    type: 'file-reject';
+}
+
 const rtcConf = {
     iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
+
+type ControlMessage = FileRequestMessage | FileAcceptMessage | FileRejectMessage;
 
 export const Session: React.FC = () => {
     const { id } = useParams();
@@ -13,11 +29,61 @@ export const Session: React.FC = () => {
     const dataChannel = useRef<RTCDataChannel | null>(null);
 
     const [messages, setMessages] = useState<string[]>([]);
-    const [inputValue, setInputValue] = useState('');
+
+    const [sendFile, setSendFile] = useState<File | null>(null);
+    const sendFileRef = useRef<File | null>(null);
+    const [incomingFileRequest, setIncomingFileRequest] = useState<{ name: string, size: number} | null>(null);
+    const [receivedChunks, setReceivedChunks] = useState<ArrayBuffer[]>([]);
+    const [receivedSize, setReceivedSize] = useState<number>(0);
+    const [transferProgress, setTransferProgress] = useState<number>(0);
+    const [isReceiving, setIsReceiving] = useState<boolean>(false);
+
+    useEffect(() => { sendFileRef.current = sendFile; }, [sendFile]);
 
     useEffect(() => {
         if (!socket || !id) {
             return;
+        }
+    
+        const handleDataChannelMessage = (event: MessageEvent) => {
+            if (typeof event.data === 'string') {
+                try {
+                    const msg = JSON.parse(event.data) as ControlMessage;
+                    switch (msg.type) {
+                        case 'file-request':
+                            setMessages(prev => [...prev, `User wants to send file: ${msg.name} (${msg.size} bytes)`]);
+                            setIncomingFileRequest({ name: msg.name, size: msg.size });
+                            break;
+                        case 'file-accept':
+                            setMessages(prev => [...prev, 'User accepted. Starting file transfer...']);
+                            sendFileInChunks();
+                            break;
+                        case 'file-reject':
+                            setMessages(prev => [...prev, 'User rejected file transfer.']);
+                            setSendFile(null);
+                            break;
+                    }
+                }
+                catch (ex) {
+                    console.error('Failed to parse conrol message', ex);
+                }
+            }
+            else {
+                const chunk = event.data as ArrayBuffer;
+                setReceivedSize(prevSize => prevSize + chunk.byteLength);
+                setReceivedChunks(prevChunks => [...prevChunks, chunk]);
+            }
+        }
+
+        const setupDataChannel = (dc: RTCDataChannel) => {
+            dc.onmessage = handleDataChannelMessage;
+            dc.onopen = () => {
+                setMessages(prev => [...prev, 'Data channel is open. You can now send files.']);
+                console.log('Data channel open.');
+            }
+            dc.onclose = () => console.log('Data channel closed.');
+            dc.binaryType = 'arraybuffer';
+            dataChannel.current = dc;
         }
 
         const createPeerConnection = (): RTCPeerConnection => {
@@ -33,13 +99,7 @@ export const Session: React.FC = () => {
             }
 
             pc.ondatachannel = (event: RTCDataChannelEvent) => {
-                const dc = event.channel;
-                dc.onmessage = (e: MessageEvent) => {
-                    setMessages((prev) => [...prev, `Peer: ${e.data}`]);
-                }
-                dc.onopen = () => console.log('Data channel open');
-                
-                dataChannel.current = dc;
+               setupDataChannel(event.channel); 
             }
 
             peerConnection.current = pc;
@@ -51,11 +111,7 @@ export const Session: React.FC = () => {
             const pc = createPeerConnection();
 
             const dc = pc.createDataChannel('file-channel');
-            dc.onmessage = (e: MessageEvent) => {
-                setMessages((prev) => [...prev, `Peer: ${e.data}`]);
-            }
-            dc.onopen = () => console.log('Data channel open');
-            dataChannel.current = dc;
+            setupDataChannel(dc);
 
             const offer = await pc.createOffer();
             await pc.setLocalDescription(offer);
@@ -106,30 +162,167 @@ export const Session: React.FC = () => {
         }
     }, [socket, id]);
 
-    const sendMessage = () => {
-        if (dataChannel.current && dataChannel.current.readyState === 'open' && inputValue) {
-            dataChannel.current.send(inputValue);
-            setMessages((prev) => [...prev, `You: ${inputValue}`]);
-            setInputValue('');
+    // -- Transfer progress --
+    useEffect(() => {
+        if (isReceiving && incomingFileRequest) {
+            const progress = receivedSize / incomingFileRequest.size * 100;
+            setTransferProgress(progress);
         }
+    }, [receivedSize, isReceiving, incomingFileRequest]);
+
+    // -- Completed file transfer --
+    useEffect(() => {
+        if (isReceiving && incomingFileRequest && receivedSize === incomingFileRequest.size && receivedSize > 0) {
+            setMessages(prev => [...prev, `File "${incomingFileRequest.name} received successfully"`]);
+
+            const fileblob = new Blob(receivedChunks);
+            const url = URL.createObjectURL(fileblob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = incomingFileRequest.name;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+            a.remove();
+
+            setReceivedChunks([]);
+            setReceivedSize(0);
+            setTransferProgress(0);
+            setIncomingFileRequest(null);
+            setIsReceiving(false);
+        }
+    }, [receivedSize, incomingFileRequest, receivedChunks, isReceiving]);
+
+    // -- Funcitons to handle files --
+    const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            setSendFile(file);
+        }
+    };
+
+    const requestFileTransfer = () => {
+        if (sendFile && dataChannel.current && dataChannel.current.readyState === 'open') {
+            const message: FileRequestMessage = {
+                type: 'file-request',
+                name: sendFile.name,
+                size: sendFile.size
+            };
+            dataChannel.current.send(JSON.stringify(message));
+            setMessages(prev => [...prev, `You: Requested to send file: ${sendFile.name}`]);
+        }  
     }
+
+    const sendFileInChunks = async () => {
+        const file = sendFileRef.current;
+        if(!file || !dataChannel.current)
+            return;
+
+        const dc = dataChannel.current;
+        const chunkSize = 16384;
+        let offset = 0;
+
+        while (offset < file.size) {
+            const slice = file.slice(offset, offset + chunkSize);
+            const buffer = await slice.arrayBuffer();
+            dc.send(buffer);
+            offset += chunkSize;
+        }
+
+        setMessages(prev => [...prev, `You: Finished sending ${file.name}`]);
+        setSendFile(null);
+    };
+
+    /*
+    const sendFileInChunks = () => {
+        if (!sendFile || !dataChannel.current)
+            return;
+
+        const dc = dataChannel.current;
+        const chunkSize = 16384;
+        let offset = 0;
+
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            if (!e.target?.result)
+                return;
+
+            const chunk = e.target.result as ArrayBuffer;
+            dc.send(chunk);
+            offset += chunk.byteLength;
+
+            if (offset < sendFile.size) {
+                readSlice(offset);
+            }
+            else {
+                setMessages(prev => [...prev, `You: Finished sending ${sendFile.name}`]);
+                setSendFile(null);
+            }
+        };
+
+        const readSlice = (o: number) => {
+            const slice = sendFile.slice(o, o + chunkSize);
+            reader.readAsArrayBuffer(slice);
+        };
+
+        readSlice(0);
+    };
+    */
+
+    const handleAccept = () => {
+        if (incomingFileRequest && dataChannel.current && dataChannel.current.readyState === 'open') {
+            setIsReceiving(true);
+            const message: FileAcceptMessage = { type: 'file-accept' };
+            dataChannel.current.send(JSON.stringify(message));
+            setMessages(prev => [...prev, 'You: Accepted the file transfer']);
+        }
+    };
+
+    const handleReject = () => {
+        if (incomingFileRequest && dataChannel.current && dataChannel.current.readyState === 'open') {
+            const message: FileRejectMessage = { type: 'file-reject' };
+            dataChannel.current.send(JSON.stringify(message));
+            setMessages(prev => [...prev, 'You: Rejected the fil transfer']);
+            setIncomingFileRequest(null);
+        }
+    };
 
     return (
         <div>
-            <h2>P2P Chat</h2>
+            <h2>File Transfer Session</h2>
             <div style={{ border: '1px solid black', height: '200px', overflowY: 'scroll', marginBottom: '10px', padding: '5px' }}>
                 {messages.map((msg, index) => (
                   <div key={index}>{msg}</div>  
                 ))}
             </div>
-            <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
-            >
-            </input>
-            <button onClick={sendMessage}>Send</button>
-        </div>
+
+            {/* -- Incoming file request -- */}
+            {incomingFileRequest && !isReceiving && (
+                <div style={{ border: '1px solid blue', padding: '10px', margin: '10px 0'}}>
+                    <p>Incoming file: <strong>{incomingFileRequest.name}</strong> ({(incomingFileRequest.size / 1024).toFixed(2)} KB)</p>
+                    <button onClick={handleAccept}>Accept</button>                    
+                    <button onClick={handleReject} style={{marginLeft: '10px'}}>Reject</button>
+                </div>
+            )}
+
+            {/* -- Send file -- */}
+            {!incomingFileRequest && (
+                <div style={{margin: '10px 0'}}>
+                    <input type="file" onChange={handleFileSelect} disabled={!!sendFile} />
+                    <button onClick={requestFileTransfer} disabled={!sendFile} style={{marginLeft: '10px'}}>Send File</button>
+                    {sendFile && <p>Selected file: {sendFile.name}</p>}
+                </div>
+            )}
+
+            {/* -- Progress Bar -- */}
+            {isReceiving && (
+                <div style={{margin: '10px 0'}}>
+                    <p>Receiving {incomingFileRequest?.name}</p>
+                    <progress value={transferProgress} max="100" style={{width:'100%'}}/>
+                    <span> {Math.round(transferProgress)}%</span>
+                </div>
+            )}
+       </div>
     );
 };
